a1800ebbc388e92c3953be56af38debc01d24201
Erin Eland <erin@erineland.com>
2016-01-11 21:01:41 +0000

    Setting all models to use MongoDb as ts their persistent data store

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -6,11 +6,6 @@ module.exports = {
   greyMatterBackendDb: {
     name: "greyMatterBackendDb",
     connector: "mongodb",
-    //database: "greymattersloopbackapi",
-    //host: "mongodb://$OPENSHIFT_MONGODB_DB_HOST",
-    //port: "$OPENSHIFT_MONGO_DB_PORT",
     url : "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":" + process.env.OPENSHIFT_MONGODB_DB_PORT + "/greymattersloopbackapi"
-    //username: "admin",
-    //password: "YDVvspjeRMyV"
   }
 };
