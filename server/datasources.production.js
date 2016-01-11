9f9dae37890bf1b9ada1c6dfd62912b5fc8b08ff
Erin Eland <erin@erineland.com>
2016-01-11 20:45:31 +0000

    Attempting another connection string

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -6,11 +6,11 @@ module.exports = {
   greyMatterBackendDb: {
     name: "greyMatterBackendDb",
     connector: "mongodb",
-    database: "greymattersloopbackapi",
-    host: "mongodb://$OPENSHIFT_MONGODB_DB_HOST",
-    port: "$OPENSHIFT_MONGO_DB_PORT",
-    //url : "mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/",
-    username: "admin",
-    password: "YDVvspjeRMyV"
+    //database: "greymattersloopbackapi",
+    //host: "mongodb://$OPENSHIFT_MONGODB_DB_HOST",
+    //port: "$OPENSHIFT_MONGO_DB_PORT",
+    url : "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":" + process.env.OPENSHIFT_MONGODB_DB_PORT + "/greymattersloopbackapi"
+    //username: "admin",
+    //password: "YDVvspjeRMyV"
   }
 };
